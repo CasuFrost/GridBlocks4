41a0749f2a22ea2b36c0a42fb68b85af9b9c6386
Marco Casu <mcasu299@gmail.com>
2023-03-11 23:00:41 +0100

    added revolver

---
+++ Weapon/Guns/Revolver/revolver_bullet.gd
@@ -0,0 +1,45 @@
+extends Node2D
+var knockBack = 5
+var damage = 50
+var active = false
+var speed = 0
+var shootingSpeed=15
+var followPos
+var cnt = 0
+var dir = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if !active:
+		hide()
+		$Area2D/CollisionShape2D.disabled=true
+		
+func _physics_process(delta):
+	if cnt >120:
+		cnt=0
+		active=false
+	
+	if active:
+		cnt+=1
+		global_position+=dir.normalized()*shootingSpeed
+	
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("enemy"):
+		body.applyKnockBack(knockBack,$Area2D.global_position.x)
+		body.applyDamage(damage)
+	active=false
+	
+func activate(pos):
+	if !active:
+		global_position=get_tree().root.get_child(0).get_node("PlayerTest").toolPosition.global_position
+		show()
+		speed = shootingSpeed
+		$Area2D/CollisionShape2D.disabled=false
+		followPos=pos
+		look_at(pos)
+		dir = global_position.direction_to(pos)
+		active=true
